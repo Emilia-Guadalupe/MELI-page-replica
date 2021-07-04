560068d9d8a4ac81beb40b40161edeef52bdb38e
Emilia <m.emilia.guadalupe@gmail.com>
2021-07-04 12:24:36 -0300

    Adding NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import './App.css';
+
+//Components
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <>
+        <NavBar />
+      </>
     </div>
   );
 }
